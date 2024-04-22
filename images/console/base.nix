3bd552a1dbc031d1c99649b4a2527bc564a617b8
Maarten Weyns <maarten.weyns@mawey.be>
2024-04-22 15:28:04 +0200

    Add icpctools user and disable sudo password prompt

--- images/console/base.nix
+++ images/console/base.nix
@@ -14,7 +14,14 @@
                 isNormalUser = true;
                 hashedPassword = "$y$j9T$l1SabA/8/ZVLzqELOwFe7.$BpKkbTYtxX45kUHTCI33uBnwHfM.AMuOjeebag9hvP1";
                 extraGroups = [ "wheel" ];
-            }
+            };
+            "icpctools" = {
+                isNormalUser = true;
+                hashedPassword = "$y$j9T$WKPftiKUOrUjjvxzUS76o/$tS8nd3ja5WZK.AAmFjiF87ihOWrDmjIaX61Bf1J7H7B";
+            };
         }
-    }
+    };
+
+    # Disable sudo password requirement
+    security.sudo.wheelNeedsPassword = false;
 }
