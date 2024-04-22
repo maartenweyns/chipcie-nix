719c3e2c8b1b9ea08adfa79607785fcc9fdf6409
Maarten Weyns <maarten.weyns@mawey.be>
2024-04-22 14:51:24 +0200

    Build basic NixOS ISO

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+    inputs = {
+        # Use unstable for flakes
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+        # Use nixos generators for generating an iso
+        nixos-generators.url = "github:nix-community/nixos-generators";
+        nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = inputs@{ self, nixpkgs, flake-utils, ... }: {
+        ## nix build .#iso
+        ## nixcfg --build-iso && nixcfg --burn-iso 00000111112222333
+        packages.x86_64-linux.iso = inputs.nixos-generators.nixosGenerate {
+            system = "x86_64-linux";
+            format = "iso";
+            specialArgs = {
+                inherit inputs;
+            };
+            modules = [
+                # Provide minimal NixOS config
+                "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+                # ./images/console.nix
+                {
+                    system.stateVersion = "23.11";
+                }
+            ];
+        };
+    };
+}
