13bfef1a24aa2e4666b1de744e6856060a7eeb89
Maarten Weyns <maarten.weyns@mawey.be>
2024-04-22 15:26:04 +0200

    Add icpcadmin user

--- images/console/base.nix
+++ images/console/base.nix
@@ -6,4 +6,15 @@
         useDHCP = true;
         hostname = "chipcie-console";
     };
+
+    users = {
+        mutableUsers = true;
+        users.users = {
+            "icpcadmin" = {
+                isNormalUser = true;
+                hashedPassword = "$y$j9T$l1SabA/8/ZVLzqELOwFe7.$BpKkbTYtxX45kUHTCI33uBnwHfM.AMuOjeebag9hvP1";
+                extraGroups = [ "wheel" ];
+            }
+        }
+    }
 }
