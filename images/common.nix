377fec9c1f1e3718c57275a48752c988fb61eaed
Maarten Weyns <maarten.weyns@mawey.be>
2024-09-12 20:58:10 +0200

    Enable nix flake support by default

--- images/common.nix
+++ images/common.nix
@@ -3,4 +3,7 @@
 {
     boot.kernelParams = [ "console=tty0" ];
     nixpkgs.config.allowUnfree = true;
+
+    # Add experimental flakes support
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
