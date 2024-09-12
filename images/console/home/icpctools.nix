c268a1c5c406648670ab4416a455857369ce9252
Maarten Weyns <maarten.weyns@mawey.be>
2024-09-12 15:50:07 +0200

    Try and fix zip downloading and unpacking

--- images/console/home/icpctools.nix
+++ images/console/home/icpctools.nix
@@ -4,17 +4,17 @@ let
     toolsVersion = "2.5.1056";
     zipFiles = [
 	{
-	    name = "presentations";
+	    name = "presentations-${toolsVersion}.zip";
             sha256 = "6d014f52e4b97c1b53acf7e028a53a65324d41bc67e5972241de606f183293d1";
 	    extractTo = "tools/presentations-${toolsVersion}";
         }
         {
-	    name = "presentationAdmin";
+	    name = "presentationAdmin-${toolsVersion}.zip";
             sha256 = "87711039cce5f6542ae31615b786c6638257072019e5201f9a310a0494450a31";
 	    extractTo = "tools/presentationAdmin-${toolsVersion}";
         }
         {
-	    name = "resolver";
+	    name = "resolver-${toolsVersion}.zip";
 	    sha256 = "bf96730983b3f42f25e753fb12c1dd227217b61a3ab20d042a158efa41af4f76";
 	    extractTo = "tools/resolver-${toolsVersion}";
         }
@@ -41,6 +41,10 @@ in {
     programs.home-manager.enable = true;
     
     home.file = files;
+
+    home.packages = with pkgs; [
+        unzip
+    ];
 
     home.activation.extractZips = lib.hm.dag.entryAfter ["writeBoundary"] ''
 	${lib.concatStringsSep "\n" (map extractZip zipFiles)}
