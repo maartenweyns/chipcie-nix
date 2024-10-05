3cbe6acbb2c129b96dea795dfe1f39344b00c217
Joshua Gort <accounts@joshuagort.com>
2024-10-05 23:02:34 +0200

    Set some system kernel/grub options

--- images/common.nix
+++ images/common.nix
@@ -1,11 +1,13 @@
 { lib, pkgs, ... }:
 
 {
-  boot.kernelParams = [ "console=tty0" ];
+  boot.kernelParams = [ "console=tty0" "consoleblank=0" "biosdevname=0" "net.ifnames=0" ];
   nixpkgs.config.allowUnfree = true;
 
   # Add experimental flakes support
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  services.journald.console = "tty1";
 
   # Enable audio support through pulseaudio
   # hardware.pulseaudio.enable = true;
