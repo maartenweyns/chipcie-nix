53f6885675faf7477ea8293fe7643820dc43c3e6
Joshua Gort <accounts@joshuagort.com>
2024-10-06 15:24:52 +0200

    Finalized scripts

--- images/contestant/scripts.nix
+++ images/contestant/scripts.nix
@@ -1,9 +1,15 @@
 { pkgs, lib, ... }:
+let
+  submitbaseurl = "https://dj.chipcie.ch.tudelft.nl";
+in
 rec {
   systemd.tmpfiles.rules = [
     "d /icpc 0755 icpcadmin icpcadmin -"
     "C+ /icpc/scripts 0755 icpcadmin icpcadmin - ${environment.etc.icpc-scripts.source}"
+    "C+ /icpc/scripts/bin/disable-turboboost_ht 0755 icpcadmin icpcadmin - ${environment.etc.disable-turboboost.source}"
+    "C+ /icpc/scripts/bin/submit 0755 icpcadmin icpcadmin - ${environment.etc.submit-client.source}"
     "Z /icpc/scripts 755 icpcadmin icpcadmin -"
+    "f /icpc/netrc 644 icpcadmin icpcadmin -"
   ];
 
   environment.etc = {
@@ -12,14 +18,28 @@ rec {
       target = "icpc/scripts";
     };
 
-
+    disable-turboboost = {
+      source = pkgs.fetchurl {
+        url = "https://raw.githubusercontent.com/DOMjudge/domjudge-scripts/main/provision-contest/disable-turboboost_ht";
+        sha256 = "1signld4w4sp0z8vypw1kcqi5442d2kwwa62syicd6hyi9rf7kkj";
+      };
+      target = "disable-turboboost_ht";
+    };
+    submit-client = {
+      source = pkgs.fetchurl {
+        url = "https://github.com/DOMjudge/domjudge/raw/main/submit/submit";
+        sha256 = "112pnchxl8x01prxam88928m1cabl1yjipmdki3747diq9k2nihb";
+      };
+    };
   };
 
+
   environment.variables.PATH = "/icpc/scripts/bin:$PATH";
+  environment.variables.SUBMITBASEURL = submitbaseurl;
 
   environment.systemPackages = with pkgs; [
-    python311Full
-    python311Packages.requests
-    python311Packages.magic
+    python312
+    python312Packages.requests
+    python312Packages.magic
   ];
 }
