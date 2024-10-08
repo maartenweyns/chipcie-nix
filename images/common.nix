41021e574a5ce3be2e6ad1fcc9cf9b7d517a13b7
Joshua Gort <accounts@joshuagort.com>
2024-10-08 14:01:51 +0200

    Stop excessing logging from Systemd and journald

--- images/common.nix
+++ images/common.nix
@@ -8,6 +8,20 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   services.journald.console = "tty1";
+  services.journald.forwardToSyslog = false;
+  services.journald.extraConfig = ''
+    ForwardToKMsg=no
+    ForwardToConsole=yes
+    ForwardToWall=no
+    TTYPath=/dev/tty1
+    Storage=volatile
+  '';
+
+  systemd.extraConfig = ''
+    ShowStatus=no
+  '';
+
+  time.timeZone = "Europe/Amsterdam";
 
   # Enable audio support through pulseaudio
   # hardware.pulseaudio.enable = true;
