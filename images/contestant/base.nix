419b3a68cf4ea38cca5ce44326b75917cae831f6
Joshua Gort <accounts@joshuagort.com>
2024-10-08 14:02:01 +0200

    Add SSH keys of Joshua and Maarten

--- images/contestant/base.nix
+++ images/contestant/base.nix
@@ -27,6 +27,10 @@
         isNormalUser = true;
         hashedPassword = "$y$j9T$l1SabA/8/ZVLzqELOwFe7.$BpKkbTYtxX45kUHTCI33uBnwHfM.AMuOjeebag9hvP1";
         extraGroups = [ "wheel" "audio" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZwqjXcMDoY9uIXUyqogh2psoBLJZEqBV7uKrUS3VTN"
+          "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC9c4U9+L1er1PFY0haDngnq4cMf79wiBZiKcvgoes4Te13U8nU8Vee5pk5FkUPj8l5ZT2kSoermajR1NcZc4WpP60Jmwoac2ymamu1OtMe78dd9KuRCjVsaNimOrWvBL0tdeouyIK6foIxxcE4NWiSRXu78VXfeytzOkwgbLNUK50gM4uKhWLWxNY35AyES+NPCFcWw9p4YuCBWkogcOzidOQTxRQyAYOFG278c4hPkvRqajCTg3zuM4jExiLXKN/5X6SNZRtlVkNJ+sQmprSzU7ASu011u/iIVtXssuarYzQJ0isl3y0M7Z77k/+1UzTgPudE91sL7HUyal4bTAY2FdEF9o2pBn43b2Ik9o4WqIA+yQvBoeX8sWoU+AyCRE6I1/tIobKjEGaj+rD2tC8uh27dPTPyW1/ZAIxvpj0atZB4bJJ8FqZK+WgzBQP1AhiplSAKfAbmj2XOe1yxFzl4cjR4LPFrNXX+7nGojbOOuMvs6GMmBJnHc5iJbBeR29M= maarten@fedora"
+        ];
       };
     };
   };
