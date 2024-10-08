3886b2c0d8b11694624d49d6c823c65ab90ae8b6
Joshua Gort <accounts@joshuagort.com>
2024-10-08 14:02:20 +0200

    Fix autologin

--- images/contestant/gui.nix
+++ images/contestant/gui.nix
@@ -6,6 +6,20 @@
       xterm.enable = true;
       xfce.enable = true;
     };
-    displayManager.lightdm.enable = true;
+    displayManager = {
+      lightdm = {
+        enable = true;
+        autoLogin.timeout = 0;
+        greeter.enable = true;
+      };
+
+    };
+  };
+
+  services.displayManager = {
+    autoLogin = {
+      enable = true;
+      user = "contestant";
+    };
   };
 }
